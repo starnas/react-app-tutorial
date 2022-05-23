b69dd658de545884f8c6e951b17fbbfe1d716d03
pstar <pst@ngc.dk>
2022-05-23 09:34:24 +0200

    added add/fetch from server

--- src/App.js
+++ src/App.js
@@ -8,26 +8,45 @@ const App = () => {
   const [tasks, setTasks] = useState([])
 
   useEffect (() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-
-      console.log(data)
+    const getTasks = async() => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
 
-    fetchTasks()
+    getTasks()
   }, [])
 
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    //const id = Math.floor(Math.random() * 10000) + 1
+    //const newTask = {id, ...task}
+    //setTasks([...tasks, newTask])
 
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'}) 
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
