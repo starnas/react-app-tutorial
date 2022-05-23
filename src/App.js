356b2e16d6a10b261bd4bc1a8c81e4061c3b8b41
pstar <pst@ngc.dk>
2022-05-23 09:43:07 +0200

    fixed reminder setting

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ const App = () => {
     return data
   }
 
+  // Fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
   // Add Task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
@@ -51,8 +59,25 @@ const App = () => {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    )
   }
 
   return (
